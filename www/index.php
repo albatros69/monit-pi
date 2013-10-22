245d36204c09f8ff220d6939657ce1f4f36dacaa
Albatros69 <albatros69+github@gmail.com>
2013-10-22 20:21:13 +0200

    Adaptation au hostname

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 ﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-		<title>Monitoring de rincevent</title>
+		<title>Monitoring of <?php echo gethostname();?></title>
 		<meta http-equiv="refresh" content="300" />
 		<meta http-equiv="pragma" content="no-cache" />
 		<meta http-equiv="cache-control" content="no-cache" />
