366c5a1482b2fefe29dcbb4ee630a7f3f32e9d9e
Albatros69 <albatros69+github@gmail.com>
2014-02-05 21:46:12 +0100

    Put more than one graph per line

--- www/index.php
+++ www/index.php
@@ -1,15 +1,16 @@
 ﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-	<head>
-		<title>Monitoring of <?php echo gethostname();?></title>
-		<meta http-equiv="refresh" content="300" />
-		<meta http-equiv="pragma" content="no-cache" />
-		<meta http-equiv="cache-control" content="no-cache" />
-		<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <head>
+        <title>Monitoring of <?php echo gethostname();?></title>
+        <meta http-equiv="refresh" content="300" />
+        <meta http-equiv="pragma" content="no-cache" />
+        <meta http-equiv="cache-control" content="no-cache" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     </head>
 
     <style>
         img { width: 100%; height: auto; max-width: 700px; }
+        div.graph { display: inline-block; margin-right: 1em; }
     </style>
 
     <body>
@@ -27,15 +28,15 @@ echo '<h1>Monitoring of '.gethostname()."</h1>\n";
 if (isset($graph)) {
     echo "<h2>{$rrds[$graph]['title']}</h2>\n";
     foreach (array_keys($periods) as $p) {
-        echo '<h3>'.ucfirst($p)." graph</h3>\n";
-        echo "<p><a href='${_SERVER['PHP_SELF']}?period=$p'><img src='graph.php?graph=$graph&amp;period=$p'></a></p>\n";
+        echo '<div class="graph"><h3>'.ucfirst($p)." graph</h3>\n";
+        echo "<p><a href='${_SERVER['PHP_SELF']}?period=$p'><img src='graph.php?graph=$graph&amp;period=$p'></a></p></div>\n";
     }
 }
 elseif (isset($period)) {
     echo '<h2>'.ucfirst($period)." graphs</h2>\n";
     foreach ($rrds as $g => $v) {
         echo "<h3>{$v['title']}</h3>\n";
-        echo "<p><a href='${_SERVER['PHP_SELF']}?graph=$g'><img src='graph.php?graph=$g&amp;period=$period'></a></p>\n";
+        echo "<div class=\"graph\"><p><a href='${_SERVER['PHP_SELF']}?graph=$g'><img src='graph.php?graph=$g&amp;period=$period'></a></p></div>\n";
     }
 }
 else {
